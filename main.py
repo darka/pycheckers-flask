681fc7f9ba99ad60b56cddae3074daca007b69f5
Darius Scerbavicius <darius.scerb@gmail.com>
2020-12-26 21:09:35 +0000

    Basic movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 
+from flask import request
 import pycheckers
 import pycheckers.svg
 
@@ -25,6 +26,11 @@ rect.board-square {
   stroke: black;
   stroke-width: 3;
 }
+rect.board-square-selected {
+  fill: #66ff33;
+  stroke: #003300;
+  stroke-width: 3;
+}
 circle.checker-red {
   fill: red;
   stroke: black;
@@ -41,21 +47,41 @@ circle.checker-black {
 
 script = """\
 <script>
+
+function selectSquare(squareId) {
+  var element = document.getElementById(squareId);
+  if (element.classList.contains("board-square-selected"))
+    return;
+  element.classList.add("board-square-selected");
+}
+
+function deselectSquare(squareId) {
+  var element = document.getElementById(squareId);
+  if (!element.classList.contains("board-square-selected"))
+    return;
+  element.classList.remove("board-square-selected");
+}
+
+function extractPos(squareId) {
+  var splt = squareId.split("-");
+  var x = parseInt(splt[2]);
+  var y = parseInt(splt[3]);
+  return [x, y];
+}
+
 class App {
   constructor() {
     this.state = {
-      x: null,
-      y: null
+      selectedSquares: []
     };
     this.render();
   }
   render() {
-    const {x, y} = this.state;
+    const {selectedSquares,} = this.state;
     fetch('/game')
         .then(function (response) {
             response.text().then(function (text) {
-                document
-                .body
+                document.getElementById("main")
                 .innerHTML = text
             });
         })
@@ -65,18 +91,37 @@ class App {
   }
 }
 
-window.onload = () => new App();
+var app = new App();
+window.onload = () => {
+  document.getElementById("move-button").addEventListener("click", function() {
+    fetch("/move", {
+      method: "POST", 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(app.state.selectedSquares)
+    })
+    .then(res => { app.render(); });
+  });
+}
 
 document.addEventListener('click', function (event) {
     if (!event.target.closest('.board-square')) return;
+    selectSquare(event.target.id);
+    var pos = extractPos(event.target.id);
+    app.state.selectedSquares.push(pos);
     console.log(event.target);
 }, false);
+
 </script>
 """
 
 def render():
-    output = f"<html><head>{style}{script}</head><body>\n"
-    return output + "</body></html>"
+    return f"""\
+<html><head>{style}{script}</head><body>
+<div id="main"></div>
+<button id="move-button">Move</button>
+</body></html>"""
 
 @app.route("/")
 def root():
@@ -85,6 +130,15 @@ def root():
 @app.route("/game")
 def render_game():
     return pycheckers.svg.render(game, 500)
+
+@app.route("/move", methods=["POST"])
+def move():
+    print(request.headers)
+    print(request.json)
+    piece_pos = request.json[0]
+    rest = request.json[1:]
+    game.move(*piece_pos, rest)
+    return "done"
 
 
 if __name__ == "__main__":
